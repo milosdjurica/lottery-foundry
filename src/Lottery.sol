40c12bbd115ed7ae6497d37a9a92b48a42c08ccf
milosdjurica <milosdjurica13@gmail.com>
2023-11-28 00:25:50 +0100

    pickWinner if not enough time passed then revert

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -11,13 +11,18 @@ contract Lottery {
     error Lottery__NotEnoughETHSent();
 
     uint private immutable i_ticketPrice;
+    // Duration of the lottery in seconds
+    uint private immutable i_interval;
     address payable[] private s_players; // payable because we have to pay the winner at the end
+    uint private s_lastTimeStamp;
 
     // Events
     event EnteredLottery(address indexed player);
 
-    constructor(uint ticketPrice) {
+    constructor(uint ticketPrice, uint interval) {
         i_ticketPrice = ticketPrice;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterLottery() external payable {
@@ -30,7 +35,17 @@ contract Lottery {
         emit EnteredLottery(msg.sender);
     }
 
-    function pickWinner() public {}
+    // Get random number
+    // Use rand num to pick a player
+    // Be automatically called
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert();
+        }
+
+        // 1. Request RNG
+        // 2. Get random number
+    }
 
     // ! Getters
     function getTicketPrice() external view returns (uint) {
