ab323ba08eb8bf65c45276656e4d6d4b8c8b7765
milosdjurica <milosdjurica13@gmail.com>
2023-11-27 22:14:01 +0100

    added conventions and started coding contract -> constructor and functions layout

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-contract Lottery {}
+/**
+ * @title A Simple Lottery Contract
+ * @author Milos Djurica
+ * @notice This contract gives user a chance to join the lottery and win total prize
+ * @dev Implements Chainlink VRFv2
+ */
+contract Lottery {
+    uint private immutable i_ticketPrice;
+
+    constructor(uint ticketPrice) {
+        i_ticketPrice = ticketPrice;
+    }
+
+    function enterLottery() public payable {}
+
+    function pickWinner() public {}
+}
