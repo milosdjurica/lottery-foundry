28cc93ed1191f9b8e1e73ebe96e3278a699a407e
milosdjurica <milosdjurica13@gmail.com>
2023-11-28 01:00:59 +0100

    implemented VRFCoordinatorV2Interface to request a random number from chainlink

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 /**
  * @title A Simple Lottery Contract
  * @author Milos Djurica
@@ -10,18 +12,38 @@ pragma solidity ^0.8.19;
 contract Lottery {
     error Lottery__NotEnoughETHSent();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     uint private immutable i_ticketPrice;
     // Duration of the lottery in seconds
     uint private immutable i_interval;
-    address payable[] private s_players; // payable because we have to pay the winner at the end
+    // ! VRF arguments
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
     uint private s_lastTimeStamp;
+    address payable[] private s_players; // payable because we have to pay the winner at the end
 
     // Events
     event EnteredLottery(address indexed player);
 
-    constructor(uint ticketPrice, uint interval) {
+    constructor(
+        uint ticketPrice,
+        uint interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) {
         i_ticketPrice = ticketPrice;
         i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
         s_lastTimeStamp = block.timestamp;
     }
 
@@ -43,6 +65,13 @@ contract Lottery {
             revert();
         }
 
+        uint requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gas lane
+            i_subscriptionId, // id
+            REQUEST_CONFIRMATIONS, // how many blocks should pass
+            i_callbackGasLimit, // gass limit
+            NUM_WORDS // number of random numbers we get back
+        );
         // 1. Request RNG
         // 2. Get random number
     }
